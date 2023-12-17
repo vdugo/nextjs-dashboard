a0d2f2de7b475693ea1629ba863004b0aa99dde3
Vincent <v3rdugo@yahoo.com>
2023-12-16 16:10:44 -0800

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import Image from 'next/image';
+
+import styles from "@/app/ui/home.module.css"
 
 export default function Page() {
   return (
@@ -17,6 +20,9 @@ export default function Page() {
             </a>
             , brought to you by Vercel.
           </p>
+          <div
+            className={styles.shape}
+          />
           <Link
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
@@ -26,6 +32,20 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className='hidden md:block'
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className='hidden md:hidden'
+            alt="Screenshots of the dashboard project showing mobile version"
+          />
         </div>
       </div>
     </main>
